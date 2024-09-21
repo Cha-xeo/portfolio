3ecc8b9c83cf7ad08b73d7ef2ed380b303ed2f2e
Cha-xeo <arnaud.lalande@epitech.eu>
2024-09-21 14:21:31 +0200

    card

--- portfolio/app/components/MobyGameList.tsx
+++ portfolio/app/components/MobyGameList.tsx
@@ -16,21 +16,25 @@ export const MobyGameList: React.FC<NormalGamesResponse> = ({ games }) => {
     }, []);
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 p-8">
+        <div className="max-h-screen overflow-y-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 p-8">
             {games.map((game: Normal) => (
                 <div key={game.game_id} className="bg-white rounded-lg shadow-lg overflow-hidden">
+                    <div className="pt-5 pb-5 bg-blue-100 rounded-lg shadow-lg overflow-hidden">
                         <a href={game.moby_url}>
                             <Image
                                 src={game.sample_cover?.image ?? ""}
                                 width={game.sample_cover?.width ?? 256}
                                 height={game.sample_cover?.height ?? 196}
                                 alt={game.title}
-                                className="w-full h-48 object-fit"
+                                className="w-full h-48 object-contain"
                             />
                         </a>
+                    </div>
+                    {/* <div className=" rounded-lg shadow-lg overflow-hidden">
+                    </div> */}
                     <div className="p-6">
                         <h2 className="text-xl font-semibold mb-4">{game.title}</h2>
-                            <GameDescription description={game.description ?? ""}/>
+                            {/* <GameDescription description={game.description ?? ""}/> */}
                             {/* <p className="text-gray-600 mb-4" dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(game.description ?? "") }}></p> */}
                         <Link href={{
                             pathname: `${game.moby_url}`,
