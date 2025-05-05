aabef949abd0e8dfe1d87515435d26814c770614
Cha-xeo <arnaud.lalande@epitech.eu>
2025-05-05 21:39:34 +0200

    moby

--- portfolio/app/mobygamesapi/[id]/page.tsx
+++ portfolio/app/mobygamesapi/[id]/page.tsx
@@ -1,11 +1,11 @@
 // import { fetchGamesById } from "@/app/api/searchGames/route";
-// import MobyGameDetails from "@/app/components/mobyGames/MobyGameDetails";
+import MobyGameDetails from "@/app/components/mobyGames/MobyGameDetails";
 
-// export default async function Page({ params }: { params: { id: number } }) {
-//     const game = await fetchGamesById(params.id)
-//     return(
-//         <div>
-//             {/* <MobyGameDetails game={game}/> */}
-//         </div>
-//     )
-// }
+export default async function Page({ params }: { params: { id: number } }) {
+    // const game = await fetchGamesById(params.id)
+    return(
+        <div>
+            {/* <MobyGameDetails game={game}/> */}
+        </div>
+    )
+}
