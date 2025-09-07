7e55d09372561c291d0a634b09a9e7a9fed7e43e
Cha-xeo <arnaud.lalande@epitech.eu>
2025-09-07 21:27:52 +0200

    Menu correction

--- portfolio/app/components/NavBar.tsx
+++ portfolio/app/components/NavBar.tsx
@@ -11,7 +11,8 @@ const NavBar = () => {
     const locale = useLocale();
 
     const t = useTranslations('NavBar');
-    const [nav, setNav] = useState(false);
+    const [isOpen, setisOpen] = useState(false);
+    const toggleMenu = () => setisOpen(!isOpen);
 
     const links = [
       {
@@ -61,15 +62,9 @@ const NavBar = () => {
             {t('title')}
           </Link>
         </h1>
-        <h1 className="text-5xl font-signature ml-2 hover:scale-105">
-        <Link
-          href="/about"
-          >
-          about
-        </Link>
-        </h1>
       </div>
       
+       {/* Desktop Menu Link */}
       <ul className="hidden md:flex">
         {links.map(({ id, link, name, type }) => (
           <li
@@ -103,49 +98,52 @@ const NavBar = () => {
           </li>
       </ul>
 
-      <div
-        onClick={() => setNav(!nav)}
-        className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
-      >
-        {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+         {/* Mobile Menu Button */}
+      <div className="sm:hidden flex justify-between items-center">
+        <span className="font-bold text-lg">Menu</span>
+        // className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
+        <button onClick={toggleMenu} aria-label="Toggle Menu">
+          {isOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
+        </button>
       </div>
 
-      {nav && (
-        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
-          {links.map(({ id, link, name, type }) => (
-            <li
+      {/* Mobile Menu Links */}
+      {isOpen && (
+        <div className="fixed inset-0 bg-gradient-to-b from-black to-gray-800 text-gray-500 bg-opacity-90 flex items-center justify-center z-50">
+          <ul className="flex flex-col items-center gap-8">
+          {/* <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500"> */}
+            {links.map(({ id, link, name, type }) => (
+              <li
               key={id}
               className="px-4 cursor-pointer capitalize py-6 text-4xl"
-            >
-              {type == "External" ? (
-              <a onClick={() => setNav(!nav)}
-                key={name}
-                href={link}
-                target="_blank"
-                rel="noopener noreferrer"
               >
-                {name}
-              </a>
-            ) : (
-              <Link onClick={() => setNav(!nav)}
+                {type == "External" ? (
+                  <a onClick={toggleMenu}
+                  key={name}
+                  href={link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  >
+                  {name}
+                </a>
+              ) : (
+                <Link onClick={toggleMenu}
                 key={name}
                 href={link}
-               >
-                {name} -{link}-
-              </Link>
-            )}
-              {/* <Link onClick={() => setNav(!nav)} href={{pathname:`/${link}`}}>
-                {name}
-              </Link> */}
+                >
+                  {name}
+                </Link>
+              )}
+              </li>
+            ))}
+            <li
+              key={links.length+1}
+              className="px-4 cursor-pointer capitalize py-6 text-4xl"
+              >
+              <LanguageSwitcher defaultValue={locale}/>
             </li>
-          ))}
-          <li
-            key={links.length+1}
-            className="px-4 cursor-pointer capitalize py-6 text-4xl"
-          >
-            <LanguageSwitcher defaultValue={locale}/>
-          </li>
-        </ul>
+          </ul>
+        </div>
       )}
     </nav>
   );
